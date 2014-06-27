6b25b22e7b56b8b571f75b6bc86d107a73b17e3c
Miro Svrtan <miro@mirosvrtan.me>
2014-06-27 22:17:05 +0200

    [Sf2SK] Added PHPUnit 4.3.* & Mockery

--- app/autoload.php
+++ app/autoload.php
@@ -9,5 +9,10 @@ use Composer\Autoload\ClassLoader;
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
+if (class_exists('PHPUnit_Runner_Version')) {
+    set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__.'/../vendor/mockery/mockery/library/');
+    require_once('Mockery/Loader.php');
+    $loader = new \Mockery\Loader;
+    $loader->register();
+}
 return $loader;
