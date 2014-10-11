9358127415054263a5bcdbe1ed025b78ad518b0f
Miro Svrtan <miro@mirosvrtan.me>
2014-10-11 20:51:10 +0200

    Added SonataAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,12 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new NullDev\UserBundle\NullDevUserBundle(),
 
+            // SonataAdmin
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
